2dcdaffe907b31962b79560a7f2d766901c6ad80
Andy March <mail@andymarch.co.uk>
2019-10-15 09:51:39 +0100

    Fixed base hook, added authorization check

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const hooks = require('./hooks')
-var router = express.Router();
 
 const app = express()
-app.use(bodyParser.json())
+const port = process.env.PORT || 4000
 
+app.use(function(req,res,next){
+  var authz = req.header("Authorization")
+  if(process.env.AUTHZ_KEY == null){
+    res.status(401).send({message: 'Access denied, authorization has not been configured.'})
+  }
+  else if(process.env.AUTHZ_KEY != authz){
+    res.status(401).send({message: 'Access denied.'})
+  }
+  next()
+})
+
+app.use(bodyParser.json())
 app.route('/').post(hooks.token_enrich)
-app.route('/').get(hooks.token_enrich)
 
 app.use(function(req,res){
   res.status(404).send({url: req.originalUrl + ' was not found'})
 })
 
-const port = process.env.PORT || 4000
-app.listen(port, () => console.log('app started on '+port))
+app.listen(port, () => console.log('Hooks started on '+port))
