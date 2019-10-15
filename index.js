d890ddcf3ac6f339c65d9fc0d827989167ec845a
Andy March <mail@andymarch.co.uk>
2019-10-15 11:08:55 +0100

    Added failed request logging

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const port = process.env.PORT || 4000
 app.use(function(req,res,next){
   var authz = req.header("Authorization")
   if(process.env.AUTHZ_KEY == null){
+    console.log("You must set AUTHZ_KEY to a value in your environment.")
     res.status(401).send({message: 'Access denied, authorization has not been configured.'})
   }
   else if(process.env.AUTHZ_KEY != authz){
+    console.log("Unauthenticated request.")
     res.status(401).send({message: 'Access denied.'})
   } else {
     next()
