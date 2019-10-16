1bf4d7f57ea7c0e7f215de2867f7f2a44af1f908
Andy March <mail@andymarch.co.uk>
2019-10-16 09:56:57 +0100

    Static claim replacement

--- hooks.js
+++ hooks.js
@@ -3,24 +3,33 @@
 exports.token_enrich = function(req, res){
     console.log(req.body.data)
 
-    var subject = JSON.parse(req.body.data).identity.sub;
+    var login = JSON.parse(req.body.data).context.session.login.split("@");
+    //00unvpfiu10vrolDp0h7@multiorg-gs.oktapreview.com#
+
+    var uuid = login[0]
+    var userDomain = login[1]
 
     var structure = {}
     var commands = 'commands'
     structure[commands] = []
 
-    if(subject != null){ 
+    if(login != null){ 
         //decompose subject and call the appropriate spoke for user data
-        
-        /*var idCommand = {
-            'type': 'com.okta.identity.patch',
-            'value': [
-                {
-                    'op': 'add',
-                    'path': '/claims/marketingIdentifier',
-                    'value': "mkt"+new Date() + getMilliseconds()
-                }
-            ]
+
+        console.log("Need to lookup user " + uuid + " of tenant " + userDomain)
+
+        var scps = JSON.parse(req.body.data).access.claims.scopes;
+        if(scps.includes("profile")){
+            var idCommand = {
+                'type': 'com.okta.identity.patch',
+                'value': [
+                    {
+                        'op': 'replace',
+                        'path': '/claims/name',
+                        'value': "Sensitive value"
+                    }
+                ]
+            }
         }
         structure[commands].push(idCommand)
         var accessCommand = {
@@ -28,12 +37,12 @@ exports.token_enrich = function(req, res){
             'value': [
                 {
                     'op': 'add',
-                    'path': '/claims/marketingPreferenceService',
-                    'value': "enable"
+                    'path': '/claims/national_id_number',
+                    'value': "12345"
                 }
             ]
         }
-        structure[commands].push(accessCommand)*/
+        structure[commands].push(accessCommand)
     }
     res.json(structure);
 };
